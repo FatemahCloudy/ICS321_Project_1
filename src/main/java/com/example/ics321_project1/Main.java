496d8cd89db3eab269775f4ed99e0502c2fa01c3
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-10-31 09:40:31 +0300

    add a header
    A header is added to the admin page and the css file is updated with the required styles.

--- src/main/java/com/example/ics321_project1/Main.java
+++ src/main/java/com/example/ics321_project1/Main.java
@@ -1,7 +1,10 @@
 package com.example.ics321_project1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -10,16 +13,32 @@ import java.net.URL;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        AdminFunctions adminFunctions = new AdminFunctions();
         GridPane pane = new GridPane();
         HBox hbox = new HBox(20);
+        HBox header = new HBox(20);
+        Scene scene = new Scene(pane, 800, 400);
+        Label title = new Label("Horse Racing Management System (Admin)");
 
-        AdminFunctions adminFunctions = new AdminFunctions();
 
+        //add header
+        header.setAlignment(Pos.CENTER);
+        header.setPadding(new Insets(15));
+        header.getStyleClass().add("header");
+
+        title.getStyleClass().add("title");
+        header.getChildren().add(title);
+
+        //add the rest of the contents
         hbox.getChildren().addAll(adminFunctions.addRace(), adminFunctions.deleteOwner(), adminFunctions.moveHorse(), adminFunctions.approveTrainer());
 
-        pane.add(hbox, 0, 0);
+        //add everything to the pane
+        pane.add(header, 0, 0);
+        pane.add(hbox, 0, 2);
 
-        Scene scene = new Scene(pane, 800, 400);
+        //add a margin between the header and the contents
+        GridPane.setMargin(hbox, new Insets(25, 0, 0, 0));
+
 
         // Load the CSS file
         URL cssUrl = getClass().getResource("/style.css");
