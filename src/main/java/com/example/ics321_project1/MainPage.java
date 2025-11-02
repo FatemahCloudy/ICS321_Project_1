6a1890d72fa7ed47b314a6a27d02f866b1e60b89
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-11-02 18:41:01 +0300

    Create the main page GUI
    Class "MainPage" is created to hold the main page GUI
    
    The style.css is updated with .vbox styling  to align the items on the centre.

---
+++ src/main/java/com/example/ics321_project1/MainPage.java
@@ -0,0 +1,59 @@
+package com.example.ics321_project1;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class MainPage extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        VBox vbox = new VBox(15);
+        HBox header = new HBox(20);
+        Scene scene = new Scene(vbox, 300, 250);
+        Label title = new Label("Horse Racing System");
+
+        // Create two buttons
+        Button button1 = new Button("Admin");
+        Button button2 = new Button("User");
+
+        //add header
+        header.setAlignment(Pos.CENTER);
+        header.setPadding(new Insets(15));
+        header.getStyleClass().add("header");
+
+        title.getStyleClass().add("title");
+        header.getChildren().add(title);
+
+        vbox.getStyleClass().add("vbox");
+
+        // add the components to the vbox
+        vbox.getChildren().addAll(header, button1, button2);
+
+
+        // load the CSS file
+        URL cssUrl = getClass().getResource("/style.css");
+        if (cssUrl != null) {
+            scene.getStylesheets().add(cssUrl.toExternalForm());
+        } else {
+            System.out.println("CSS file not found");
+        }
+
+        // set the scene on the stage
+        primaryStage.setTitle("Horse Racing");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
