2299280cbf966697baaaabce2837be19f2ce2391
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-11-02 19:13:30 +0300

    Create UserPage.java
    It is empty now. Created for testing purposes and should be implemented later.

---
+++ src/main/java/com/example/ics321_project1/UserPage.java
@@ -0,0 +1,9 @@
+package com.example.ics321_project1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UserPage extends Application {
+    @Override
+    public void start(Stage primaryStage) {}
+}
