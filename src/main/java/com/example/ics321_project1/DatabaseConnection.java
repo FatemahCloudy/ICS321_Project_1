7943641c9b486b5ec9dc60dd17523244e22d153f
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-11-03 18:39:18 +0300

    Update DatabaseConnection.java

--- src/main/java/com/example/ics321_project1/DatabaseConnection.java
+++ src/main/java/com/example/ics321_project1/DatabaseConnection.java
@@ -2,18 +2,21 @@ package com.example.ics321_project1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/horse_racing";
+
+    private static final String URL = "jdbc:mysql://localhost/horse_race";
     private static final String USER = "root";
     private static final String PASSWORD = ""; //ToDo: Change it
 
     public static Connection connect() {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("Database connection failed: " + e.getMessage());
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to MySQL successfully!");
+            return conn;
+        } catch (Exception e) {
+            System.err.println("Database connection failed: " + e.getMessage());
             return null;
         }
     }
